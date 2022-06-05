36396e64f3e5060a81cf8d6da092a89bf3d71812
nickpuetz1995 <nick.puetz@kellogg.northwestern.edu>
2022-06-05 23:38:30 +0000

    add all authentication and authorization code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,23 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({"email" => params["email"]})
+    if @user
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+        redirect_to "/places"
+      else 
+        redirect_to "/sessions/new"
+      end
+    else
+      redirect_to "/sessions/new"
+    end
+
   end
 
   def destroy
+    session["user_id"] = nil
+    redirect_to "/sessions/new"
   end
 end
   
